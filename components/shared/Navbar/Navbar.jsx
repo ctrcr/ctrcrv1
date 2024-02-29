df1358c6144d4be27cbecd02f0b25d1478c43447
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-29 14:27:00 +0530

    create navbar draft

--- components/shared/Navbar/Navbar.jsx
+++ components/shared/Navbar/Navbar.jsx
@@ -3,32 +3,49 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="items-center h-16">
+          <div className="flex items-center justify-between">
             <div className="flex-shrink-0">
               <Link href="/">
-                <span className="text-white font-bold text-xl">Logo</span>
+                <span className="text-white font-bold text-xl">CTRCR</span>
               </Link>
             </div>
+
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link href="/about">
-                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    About
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium">
+                    Home
+                  </span>
+                </Link>
+                <Link href="/about">
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-lg font-medium">
+                    Journal
+                  </span>
+                </Link>
+                <Link href="/about">
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md textlg font-medium">
+                    Events
                   </span>
                 </Link>
                 <Link href="/services">
-                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Services
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md textlg font-medium">
+                    Team
                   </span>
                 </Link>
                 <Link href="/contact">
-                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Contact
+                  <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md textlg font-medium">
+                    ESG
                   </span>
                 </Link>
               </div>
+            </div>
+
+            <div className="mt-1">
+              <Link href="/">
+                <img src="btn.svg" />
+              </Link>
             </div>
           </div>
         </div>
