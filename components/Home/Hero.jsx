a004dd3276cd1d147ee2541e9fa4cba0c4f2ff4c
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-29 21:41:18 +0530

    add event section draft1

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -3,25 +3,32 @@ import backgroundImage from "../../public/home_background.svg";
 
 const Hero = () => {
   return (
-    <section className="hero bg-cover bg-center h-screen flex items-center justify-center text-white text-2xl">
-      <div
-        style={{
-          // use the src property of the image object
-          backgroundImage: `url(${backgroundImage.src})`,
-          // other styles
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <h1 style={{ fontSize: 50, color: "white" }}>Welcome to Ctrcr!</h1>
-      </div>
-    </section>
+    <>
+      <section className="hero bg-cover bg-center h-screen flex items-center justify-center text-white text-2xl">
+        <div
+          style={{
+            // use the src property of the image object
+            backgroundImage: `url(${backgroundImage.src})`,
+            // other styles
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h1 style={{ fontSize: 50, color: "white" }}>Welcome to Ctrcr!</h1>
+        </div>
+      </section>
+      <section className="flex items-center justify-center text-white text-2xl">
+        <div className="-mt-3">
+          <img src="landing1.svg" />
+        </div>
+      </section>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      bgImage: {
-        src: "./public/home_background.svg",
-      },
     },
   },
   plugins: [],
