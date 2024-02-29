a40d218431e15d0481d4efc203a941cb12e6e714
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-29 14:49:56 +0530

    add home background image

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import backgroundImage from "../../public/home_background.svg";
 
 const Hero = () => {
   return (
     <section className="hero bg-cover bg-center h-screen flex items-center justify-center text-white text-2xl">
-      <div>
-        <h1>Welcome to our website</h1>
-        <p>Discover amazing things here</p>
+      <div
+        style={{
+          // use the src property of the image object
+          backgroundImage: `url(${backgroundImage.src})`,
+          // other styles
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ fontSize: 50, color: "white" }}>Welcome to Ctrcr!</h1>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      bgImage: {
+        src: "./public/home_background.svg",
+      },
     },
   },
   plugins: [],
