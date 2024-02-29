404de815761b7f53d5c9d87d4922c3faeb581616
Shantanu993 <shantanugahtori4@gmail.com>
2024-02-29 23:53:14 +0530

    fix landing page background image not loading

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -5,30 +5,11 @@ const Hero = () => {
   return (
     <>
       <section className="hero bg-cover bg-center h-screen flex items-center justify-center text-white text-2xl">
-        {/* <div
-          style={{
-            // use the src property of the image object
-            backgroundImage: `url(${backgroundImage.src})`,
-            // other styles
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <h1 style={{ fontSize: 50, color: "white" }}>Welcome to Ctrcr!</h1>
-          
-        </div> */}
-
         <div
-          style={{ backgroundImage: `url(${backgroundImage})` }}
-          className="h-screen w-screen bg-cover bg-center flex items-center justify-center text-white text-2xl"
+          style={{ backgroundImage: `url(${backgroundImage.src})` }}
+          className="h-screen w-screen bg-cover bg-center flex items-center justify-center bg-no-repeat text-white text-2xl"
         >
-          <h1 classname="text-[32px] text-white">Welcome to Ctrcr!</h1>
+          <h1 className="text-[32px] text-white">Welcome to Ctrcr!</h1>
         </div>
       </section>
       <section className="flex items-center justify-center text-white text-2xl">
