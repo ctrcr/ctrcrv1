a2f9a355edbe08640c115abf4c7fed319621834d
Siddharth <siddharthrai610@gmail.com>
2024-03-02 23:18:42 +0530

    update footer

--- components/shared/Footer/Footer.jsx
+++ components/shared/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-black text-white p-4">
       <div className="container mx-auto text-center">
         <p className="mb-2">CTCRC.com</p>
-        <p>©2022</p>
+        <p>©2024</p>
       </div>
     </footer>
   );
