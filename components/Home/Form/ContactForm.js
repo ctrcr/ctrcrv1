1aa0b0d93ea1612f977f804ee9df4dbb5158d85d
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-07 21:03:10 +0530

    contact form update

--- components/Home/Form/ContactForm.js
+++ components/Home/Form/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormFooter from "./FormFooter";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
@@ -54,80 +55,91 @@ export default function Contact() {
   };
 
   return (
-    <div className="w-[20vw]">
-      <h1>Contact form</h1>
+    <div className="w-[22vw] ">
       {formSuccess ? (
         <div>{formSuccessMessage}</div>
       ) : (
-        <form
-          method="POST"
-          action="https://www.formbackend.com/f/664decaabbf1c319"
-          onSubmit={submitForm}
-        >
-          <div className="mb-4">
-            <input
-              type="text"
-              name="name"
-              placeholder="Name *"
-              onChange={handleInput}
-              value={formData.name}
-              required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-            />
+        <div>
+          <div className="flex flex-col gap-y-4 mb-6">
+            <h1 className="text-5xl font-bold">
+              Get in <span className="text-gray-600">Touch</span>
+            </h1>
+            <div>
+              <h2 className="text-sm">NLU Mumbai</h2>
+              <h2 className="text-sm">Journal</h2>
+            </div>
           </div>
-
-          <div className="mb-4">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              onChange={handleInput}
-              value={formData.email}
-              // required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-            />
-          </div>
-
-          <div className="mb-4">
-            <input
-              type="tel"
-              name="phone"
-              placeholder="Phone Number *"
-              onChange={handleInput}
-              value={formData.phone}
-              required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-            />
-          </div>
-
-          <div className="mb-4">
-            <input
-              type="text"
-              name="subject"
-              placeholder="Subject"
-              onChange={handleInput}
-              value={formData.subject}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-            />
-          </div>
-
-          <div className="mb-4">
-            <textarea
-              name="message"
-              placeholder="Type your message here"
-              onChange={handleInput}
-              value={formData.message}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
-            ></textarea>
-          </div>
-
-          <button
-            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2"
-            type="submit"
+          <form
+            method="POST"
+            action="https://www.formbackend.com/f/664decaabbf1c319"
+            onSubmit={submitForm}
           >
-            Send message
-          </button>
-        </form>
+            <div className="mb-4">
+              <input
+                type="text"
+                name="name"
+                placeholder="Name *"
+                onChange={handleInput}
+                value={formData.name}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleInput}
+                value={formData.email}
+                // required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Phone Number *"
+                onChange={handleInput}
+                value={formData.phone}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+              />
+            </div>
+
+            <div className="mb-4">
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                onChange={handleInput}
+                value={formData.subject}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+              />
+            </div>
+
+            <div className="mb-4">
+              <textarea
+                name="message"
+                placeholder="Type your message here"
+                onChange={handleInput}
+                value={formData.message}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md text-sm"
+              ></textarea>
+            </div>
+
+            <button
+              className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 w-[100%]"
+              type="submit"
+            >
+              Send message
+            </button>
+          </form>
+          <FormFooter />
+        </div>
       )}
     </div>
   );
