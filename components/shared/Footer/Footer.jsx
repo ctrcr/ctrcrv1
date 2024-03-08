7f4af975f02b286f605cf17067c55443f3d1506c
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-08 10:08:52 +0530

    update footer

--- components/shared/Footer/Footer.jsx
+++ components/shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#0F111F] h-[15vh] py-4">
       <div className="flex flex-col items-center justify-center gap-1">
-        <p>sample text</p>
+        <a href="/">CTRCR.com</a>
         <p>&#64;2024</p>
       </div>
     </footer>
