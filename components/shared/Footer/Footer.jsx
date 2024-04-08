eb7c272a39bab04f39fbf72b7dbf3a37c26d1faa
WraitH17 <saranshsinha084@gmail.com>
2024-04-08 22:27:24 +0530

    :pencil2: Fix typo

--- components/shared/Footer/Footer.jsx
+++ components/shared/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-black text-white p-4">
       <div className="container mx-auto text-center">
-        <p className="mb-2">CTCRC.com</p>
+        <p className="mb-2">ctrcr.com</p>
         <p>©2024</p>
       </div>
     </footer>
