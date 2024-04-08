962f038f4531c07d7e60828ad04a0f4d49e2695d
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2024-04-09 01:09:57 +0530

    about journal push

---
+++ utils/models/events.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const eventsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  regLink: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Events = mongoose.model("Events", eventsSchema);
+
+export default Events;
