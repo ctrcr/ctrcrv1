7a7237e7995987101a6cbd2e7049e7728723f7c0
saranshsinhaa <saranshsinha084@gmail.com>
2024-10-19 16:30:32 +0530

    ♻️ Refactor teams page

--- pages/team.js
+++ pages/team.js
@@ -1,35 +1,7 @@
-import TeamLanding from "@/components/Team/Landing";
-import TeamCards from "@/components/Team/TeamCards";
-import Timeline from "@/components/Team/Timeline";
 import React from "react";
 
 const TeamPage = () => {
-  const teamMembers = [
-    { name: "John Doe", role: "Developer" },
-    { name: "Jane Smith", role: "Designer" },
-    { name: "Mike Johnson", role: "Project Manager" },
-    { name: "Sara Brown", role: "Marketing" },
-  ];
-
-  return (
-    <div className="font-montserrat ">
-      {/* <h1 className="text-2xl font-bold mb-4">Our Team</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="bg-gray-200 p-4">
-            <h2 className="text-lg font-semibold">{member.name}</h2>
-            <p className="text-gray-600">{member.role}</p>
-          </div>
-        ))}
-      </div> */}
-      <TeamLanding />
-      <div className="absolute bottom-10 hidden lg:flex justify-center items-center w-full">
-        <hr className="bg-black w-[70vw] h-1" />
-      </div>
-      <Timeline />
-      <TeamCards />
-    </div>
-  );
+  return <div className="font-montserrat "></div>;
 };
 
 export default TeamPage;
