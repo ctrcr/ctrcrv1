2c6e51c4c62d441703ed4e31d7518d00699d71f5
saranshsinhaa <saranshsinha084@gmail.com>
2024-11-07 20:26:43 +0530

    💄 Add mnlu in hero

--- components/Home/HeroContent/Content.js
+++ components/Home/HeroContent/Content.js
@@ -1,15 +1,21 @@
 import React from "react";
+import mnlu from "@/public/mnlu_logo.png";
+import ctrcr from "@/public/ctrcr_pfp.png";
+import Image from "next/image";
 
 const Content = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-black max-sm:px-8 sm:w-[65vw] gap-4 sm:gap-10 lg:text-2xl md:text-xl sm:text-lg text-base">
+    <div className="flex flex-col items-center justify-center text-black max-sm:px-8 sm:w-[65vw] gap-4 sm:gap-10 max-md:-mt-8 lg:text-2xl md:text-xl sm:text-lg text-base">
       <div className="flex flex-col items-center text-center justify-center gap-3">
-        <h1 className="lg:text-5xl md:text-4xl text-3xl font-serif font-bold">
-          Welcome to the Center for Training and Research in
+        <div className="flex items-center gap-40 max-md:gap-24 mb-4">
+          <Image src={mnlu} alt="MNLU" width={70} />
+          <Image src={ctrcr} alt="CTRCR" width={70} />
+        </div>
+        <h1 className="lg:text-5xl md:text-4xl max-md:text-center text-3xl font-serif font-bold">
+          Welcome to the Center for Training and Research in Commercial
+          Regulations
         </h1>
-        <h1 className="lg:text-5xl md:text-4xl text-3xl font-serif font-bold">
-          Commercial Regulations
-        </h1>
+        <h1 className="lg:text-5xl md:text-4xl text-3xl font-serif font-bold"></h1>
       </div>
       <h2 className="max-sm:text-lg font-serif font-bold">
         Uncovering the Latest Findings
