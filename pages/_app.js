71762967a99447eb69db9a6e2d0305c729fc73e4
saranshsinhaa <saranshsinha084@gmail.com>
2024-11-09 21:30:40 +0530

    💄 Fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
         <title>
           Center for Training and Research in Commercial Regulations | MNLU
         </title>
-        <link rel="icon" href="/ctrcr_pfp.png" />
+        <link rel="icon" href="/ctrcr_logo.png" />
         <meta
           name="title"
           content="Center for Training and Research in Commercial Regulations, Maharashtra National Law University, Mumbai"
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://ctrcr.com" />
         <link rel="canonical" href="https://ctrcr.com" />
 
-        <meta property="image" content="../public/ctrcr_pfp.png" />
+        <meta property="image" content="../public/ctrcr_logo.png" />
 
         <meta
           property="og:site_name"
@@ -48,7 +48,7 @@ export default function App({ Component, pageProps }) {
           content="Center for Training and Research in Commercial Regulations, Maharashtra National Law University, Mumbai"
         />
         <meta property="og:url" content="https://ctrcr.com" />
-        <meta property="og:image" content="/public/ctrcr_pfp.png" />
+        <meta property="og:image" content="/public/ctrcr_logo.png" />
         <meta property="og:type" content="website" />
         <meta property="og:see_also" content="https://ctrcr.com" />
 
